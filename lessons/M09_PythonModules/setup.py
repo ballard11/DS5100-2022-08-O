30b511477f711ab1d4025f7e46d6a1d445e768ee
Ben <ben.g.ballard@gmail.com>
2022-11-02 21:26:17 -0400

    Pushing
    Adding files to the boarder repository

---
+++ lessons/M09_PythonModules/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='HW_package',
+  version='1.0.0'
+  url='https://github.com/ballard11/DS5100-2022-08-O/tree/main/lessons/M09_PythonModules/HW9_Package'
+  author='Ben Ballard'
+  author_email='bkq5nt@virginia.edu'
+  description='This is a package with a class made to track my favorite books'
+  packages=find_packages(),
+)
