134a5db0cc561e76c3cbf0f128bef6432085f7b2
Ben <ben.g.ballard@gmail.com>
2022-11-03 23:09:57 -0400

    Add files via upload

---
+++ lessons/M10_RBasics/In_Class.R
@@ -0,0 +1,32 @@
+
+Fib <- numeric(10)
+Fib[1] <- Fib[2] <- 1
+for (i in 30) {
+  Fib[i] <- Fib[i-2] + Fib[i-1]
+}
+
+
+unit_interval <- function(x){
+  if (x >= 0 && x <= 1 ) {
+    return(1)
+    
+  }
+  else(0){
+    return(0)
+  }
+  
+}
+
+
+
+
+for ( n in )
+  
+  fizz
+
+else if
+
+else if
+
+else
+  
